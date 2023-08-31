845bb34de9ce3ee64bf3e441122779bc0f233a6d
alyorjon <alyorjon>
2023-08-31 13:26:35 +0500

    first commit

---
+++ interpalatsiya.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// let channel = prompt('Give me youtuber url')
+// console.log(channel)
+
+// let new_value=prompt('Give me something')
+// console.log(`Your input is ${new_value}`)
+
+
+let next1 = "Alyor";
+let next2 = "Donyort";
+console.log(`My name is ${next1},my bro's name is ${next2} `)
---
+++ usejs.js
@@ -0,0 +1,19 @@
+"use strict";
+// let number =10;
+// console.log(number)
+
+// let obj ={
+//     id:1,name:"Alyor",age:26
+// };
+// console.log(obj.age);
+
+
+// let curruntPlace = prompt("Qayerdasab","Logon");
+// console.log(curruntPlace);
+
+let new_list = []
+new_list[0]=prompt("Isming nima")
+new_list[1]=prompt("Yoshing nechida")
+new_list[2]=prompt("Yashash joying nechida")
+
+console.log(new_list)
